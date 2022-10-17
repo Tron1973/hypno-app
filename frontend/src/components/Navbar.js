90ac3f34f4b0f592c4679fe9be6fb34a0df3e641
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-10-17 09:52:15 -0700

    added auth context

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import { NavbarBackground, NavbarContainer, LeftContainer, RightContainer, NavbarInnerContainer, MobileMenu, NavbarLinkContainer, NavbarLink, NavbarLinkMobile, Logo, MenuButton } from '../styles/Navbar.style'
 import LogoImg from '../assets/HypnoLogo.png'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false)
   const { logout } = useLogout()
+  const { user } = useAuthContext()
   const handleClick = () => {
     logout()
   }
@@ -24,15 +26,22 @@ const Navbar = () => {
 
       <RightContainer>
         <NavbarLinkContainer>
+        { user && (
           <div>
+            <span>{user.email}</span>
             <button onClick={handleClick}>Log Out</button>
           </div>
-        <NavbarLink to='/login'>Login</NavbarLink>
-        <NavbarLink to='/signup'>Signup</NavbarLink>
-        <MenuButton onClick={() => {
-          setMobileMenu((toggle) => !toggle);}}>
-          { mobileMenu ? <>&#10005;</> : <>&#8801;</>}
-        </MenuButton>
+        )}
+        { !user && (
+          <>
+          <NavbarLink to='/login'>Login</NavbarLink>
+          <NavbarLink to='/signup'>Signup</NavbarLink>
+          <MenuButton onClick={() => {
+            setMobileMenu((toggle) => !toggle);}}>
+            { mobileMenu ? <>&#10005;</> : <>&#8801;</>}
+          </MenuButton>
+          </>
+        )}
         </NavbarLinkContainer>
       </RightContainer>
       </NavbarInnerContainer>
